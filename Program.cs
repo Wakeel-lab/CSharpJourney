3963cf61340db989d49401e87c6496c2c1605f7d
Wakeel Saliu <wakeelsaliu1@gmail.com>
2021-08-09 18:51:07 +0100

    GenerateOTP

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharpJourney
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] allowedCharacters = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            Console.WriteLine(GenerateRandomOTP(5, allowedCharacters)); 
+        }
+
+        public static string GenerateRandomOTP(int OTPLength, string[] allowedCharacters)
+        {
+            string OTP = string.Empty;
+            string tempCharacters = "";
+
+            Random randomNumbers = new Random();
+
+            for (int i = 0; i < OTPLength; i++)
+            {
+                int randomOTP = randomNumbers.Next(0, allowedCharacters.Length);
+                tempCharacters = allowedCharacters[randomOTP];
+                OTP += tempCharacters;
+            }
+
+            return OTP;
+        }
+    }
+}
